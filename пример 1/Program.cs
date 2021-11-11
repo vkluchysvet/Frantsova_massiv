c4d8cb5cf9a53290a250ea559e6cad3ae199138c
vkluchysvet <svetlanatylerjoshya@gmail.com>
2021-11-11 14:01:25 +0300

    Добавьте файлы проекта.

---
+++ пример 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пример_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявляем двумерный массив
+            int[,] myArr = new int[4, 5];
+            Random ran = new Random();
+            //Инициализируем данный массив
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    myArr[i, j] = ran.Next(1, 15);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();            
+        }
+    }
+}
